62440fb42176dfd8e068fd56279a23b2e901a1d5
PinedaVictor <pinedavictor095@gmail.com>
2022-06-13 09:38:07 -0700

    Added Spark dependency

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,12 +2,15 @@ import user.User;
 import user.HairColor;
 import data.TestingData;
 
+import org.apache.spark
+
+
 @main def main = {
   println("In the main funciton");
   println("Calling function");
   val oneUser = TestingData.generateUser();
   println(oneUser.getFullName());
-  val multipleUsers = TestingData.generateUserData(100);
+  val multipleUsers = TestingData.generateUserData(12);
   println("Number of users: " + multipleUsers.length);
 
   for i <- multipleUsers yield println(s"${i.getAge()} ${i.getFullName()} ${i.getUserID()}");
